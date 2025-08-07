63f8ecff194c11b517d26a4b65ed18700544afbc
won0845 <tpqms775@gmail.com>
2025-08-07 15:42:12 +0900

    test : 테스트 controller 추가
    1. redis 연결 체크
    2. rds 연결 체크

---
+++ src/main/java/com/example/unicon/controller/testController.java
@@ -0,0 +1,18 @@
+package com.example.unicon.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class testController {
+    @Value("${TEST_VALUE}")
+    private int httpPort;
+
+     @GetMapping("/test")
+     public String testEndpoint() {
+         System.out.println("HTTP Port: " + httpPort);
+         return "This is a test endpoint";
+     }
+}
